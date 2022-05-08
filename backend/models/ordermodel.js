989a5ddd777fe4fd5ecbfdc33f12cc843eda816d
manjeetdhayal <manjeetdhayal786@gmail.com>
2022-05-08 15:51:20 +0530

    Schema for Order object is defined

--- backend/models/ordermodel.js
+++ backend/models/ordermodel.js
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
+//schema for order
 const orderSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'User',
+      ref: "User",
     },
     orderItems: [
       {
@@ -16,7 +17,7 @@ const orderSchema = mongoose.Schema(
         product: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
-          ref: 'Product',
+          ref: "Product",
         },
       },
     ],
@@ -71,8 +72,8 @@ const orderSchema = mongoose.Schema(
   {
     timestamps: true,
   }
-)
+);
 
-const Order = mongoose.model('Order', orderSchema)
+const Order = mongoose.model("Order", orderSchema);
 
-export default Order
+export default Order;
--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,46 @@
-// const express = require('express') //it is common js 
+// const express = require('express') //it is common js
 // const products = require('./data/products')
 //now we can use es modules after node v14 (both above are correct)
 
-// converting above commonJs in ES6 module 
-import express from 'express'; 
-import dotenv from 'dotenv'; 
+// converting above commonJs in ES6 module
+import express from "express";
+import dotenv from "dotenv";
 // import products from './data/products.js'
-import connectDB from './config/db.js'
+import connectDB from "./config/db.js";
 
+import productRoutes from "./Routes/productRoutes.js";
 
-import productRoutes from './Routes/productRoutes.js'
+// const dotenv = require('dotenv');
 
+dotenv.config();
 
-// const dotenv = require('dotenv'); 
+connectDB(); //to conncect to database mongoDB
 
-dotenv.config(); 
+const app = express();
 
-connectDB(); 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-const app = express(); 
-
-
-app.get('/', (req, res)=> {
-    res.send('API is running...')
-}); 
-
-
-app.use('/api/products', productRoutes) //we are mounting the api end point i.e, now for all api calls it will take /api/products/ as default i.e, it is basically mounting it
-
+app.use("/api/products", productRoutes); //we are mounting the api end point i.e, now for all api calls it will take /api/products/ as default i.e, it is basically mounting it
 
 //below are moved to backend/Routes/productRoutes.js for better clearity and understandibility
 
 // app.get('/api/products', (req, res)=> {
-//     res.json(products); 
-// }); 
+//     res.json(products);
+// });
 
-
-// //to get the variable id param we use req.params.id from the url 
+// //to get the variable id param we use req.params.id from the url
 
 // app.get('/api/products/:id', (req, res)=> {
-    
+
 //     const product = products.find( p => p._id === req.params.id)
 //     res.json(product)
 // })
 
-const PORT = process.env.PORT || 5000 
+const PORT = process.env.PORT || 5000;
 
-app.listen( PORT, console.log(`server running in ${process.env.NODE_ENV} mode on ${PORT}`)); 
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+);
