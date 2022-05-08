36720426d096784cb27fe6bf1783da2d06a52cca
manjeetdhayal <manjeetdhayal786@gmail.com>
2022-05-08 16:49:59 +0530

    cleaning code, transferring errorhandlers to different folder

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ import connectDB from "./config/db.js";
 
 import productRoutes from "./Routes/productRoutes.js";
 
+//importing error handlers
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 // const dotenv = require('dotenv');
 
 dotenv.config();
@@ -39,19 +42,9 @@ app.use("/api/products", productRoutes); //we are mounting the api end point i.e
 // })
 
 //handling exception error when url enterd is incorrect
-app.use((req, res, next) => {
-  const error = new Error(`Not Found -${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
+app.use(notFound);
 //custom error handler COMPLEX CONCEPT
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-});
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
