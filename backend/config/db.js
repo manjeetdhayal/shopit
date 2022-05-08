c0c5308722b80f5c32c95b655cfaca62321dd8c6
manjeetdhayal <manjeetdhayal786@gmail.com>
2022-05-08 15:45:45 +0530

    User, Product, Order Schema defined

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,14 @@
-import mongoose from 'mongoose'; 
+import mongoose from "mongoose";
 
-const connect = async ()=> {
-    try{
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-        console.log(`MongoDB connected: ${conn.connection.host}`)
-    }
-    catch (error) {
-        console.log(`Error: ${error.message}`); 
-        process.exit(1)
-    }
-}
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
-export default connect 
+export default connectDB;
--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -1,32 +1,33 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
+//define schema for a user also known as modeling data schema
 const userSchema = mongoose.Schema(
-    {
-      _id: Object, 
-      name: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-      isAdmin: {
-        type: Boolean,
-        required: true,
-        default: false,
-      },
+  {
+    _id: Object,
+    name: {
+      type: String,
+      required: true,
     },
-    {
-      timestamps: true,
-    }
-  )
-  
-const User = mongoose.model('User', userSchema); 
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default User; 
+const User = mongoose.model("User", userSchema);
+
+export default User;
