959fd326f51a2fd524aa93a1a2e5b10c34a49c94
manjeetdhayal <manjeetdhayal786@gmail.com>
2022-05-08 16:15:19 +0530

    Fetching product from the database

--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -1,38 +1,39 @@
-import express from 'express'
-import expressAsyncHandler from 'express-async-handler';
-const router = express.Router(); 
+import express from "express";
+import expressAsyncHandler from "express-async-handler"; //simple middleware for handling exceptions inside of async express routes and passing them to express error handlers
+const router = express.Router();
 
-import Product from '../models/productmodel.js'
+import Product from "../models/productmodel.js";
 
-// let's a header for each routes
+// let's add a header for each routes
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
 
-router.get('/', expressAsyncHandler(async (req, res)=> {
+router.get(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
-    const products = await Product.find({})
-    res.json(products); 
-})); 
+//to get the variable id param we use req.params.id from the url
 
-
-//to get the variable id param we use req.params.id from the url 
-
-// @desc Fetch single all products
-// @route GET /api/products
+// @desc Fetch single product
+// @route GET /api/products/:id
 // @access Public
 
+router.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-
-router.get('/:id', expressAsyncHandler( async(req, res)=> {
-    
-    const product = await Product.findById( req.params.id); 
-
-    if(product) {
-        res.json(product)
+    if (product) {
+      res.json(product);
     } else {
-        res.status(404).json({message: 'Product not found'})
+      res.status(404).json({ message: "Product not found" });
     }
-}))
+  })
+);
 
-export default router; 
+export default router;
