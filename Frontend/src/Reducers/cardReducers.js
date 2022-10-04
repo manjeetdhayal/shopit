86ff3f40b8c2cbcb4e17f9d3ce5f37296a2f72f4
Manjeet <manjeet@Manjeets-MacBook-Air.local>
2022-10-04 14:38:43 +0530

    Cart: Add to cart functionality added

---
+++ Frontend/src/Reducers/cardReducers.js
@@ -0,0 +1,26 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cardConstants";
+
+export const cartReducer = (state = {cartItems: []}, action) => {
+    switch(action.type) {
+        case CART_ADD_ITEM: 
+            const item = action.payload
+            // we check if the item already exists or not 
+            const existItem = state.cartItems.find(x => x.product === item.product); 
+            if(existItem) {
+                //if it exists 
+                return {
+                    ...state, 
+                    cartItems: state.cartItems.map( x => x.product === existItem.product ? item: x ), 
+                }
+
+            } else {
+                // if not exists add current item to the state 
+                return {...state, cartItems: [...state.cartItems, item]} // in cartitems add all previous item + new item 
+                // we used spread operator to add all previous items [...state.cartItems] basically spreads all the previous items
+            }
+
+
+        default: return state; 
+
+    }
+}
---
+++ Frontend/src/actions/cartActions.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { CART_ADD_ITEM } from '../constants/cardConstants'
+
+//async is used with the help of redux thunk that applies middleware and allows us to make it async call 
+export const addToCart = (id, qty) => async(dispatch, getState) => {
+    const {data } = await axios.get(`/api/products/${id}`); 
+
+    //once we have the data we dispatch the payload 
+    dispatch({
+        type: CART_ADD_ITEM, 
+        payload: {
+            product: data._id, 
+            name: data.name, 
+            image: data.image, 
+            price: data.price,
+            countInStock: data.countInStock, 
+            qty // this from the parameters or say props 
+        }
+    })
+
+    //once we dispatch it we save our cart to the local storage so that we can use it directly form there 
+    //only string JSON data is saved in the local storage. Thus we convert it into that format before saving it. 
+    // getState().cart.cartItems => this will give us JavaScript object 
+    // we convert it to json format before saving 
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems)); //saves data in local storage 
+}
